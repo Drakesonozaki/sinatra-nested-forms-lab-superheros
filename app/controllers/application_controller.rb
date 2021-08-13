7bd56ecf5935152773e7494252eace0af6945ffb
Drakesonozaki <josephortizbw@gmail.com>
2021-08-13 08:14:10 -0600

    commit tree ff5ba41e4da7e3a856de35e79801fc986a4b0411

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
-require 'sinatra/base'
+# require '../../config/environment'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @hero_name = []
+        @hero_power = []
+        @hero_bio = []
+        @team_members = params[:team][:members]
+        @team_members.each do |hero| 
+            @hero_name << hero[:name] #adds values to the hero name param
+            @hero_power << hero[:power] #adds values to the hero power param
+            @hero_bio << hero[:bio] #adds values to the hero bio param
+        end
 
+    erb :team
+    end
 end
